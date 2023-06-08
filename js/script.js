28a70475a219370f89fa8c25a2bd1298f8a4276e
WilliamFilh0 <williamfilho8908@gmail.com>
2023-06-08 11:10:46 -0300

    Função de pular

---
+++ js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const mario = document.querySelector('.mario');          
+  const pipe = document.querySelector('.pipe');          
+
+
+  const jump = () => {
+    mario.classList.add('jump');        
+    
+    setTimeout(() => {
+  
+      mario.classList.remove('jump')
+  
+   
+    }, 500)   
+  }
+
+  
+  
+  document.addEventListener('keydown', jump);
+});
+  
