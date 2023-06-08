06664121f676fce337baf232365713a81a6a1c9e
WilliamFilh0 <williamfilho8908@gmail.com>
2023-06-08 11:28:39 -0300

    Função Stop Atualizada

--- js/script.js
+++ js/script.js
@@ -17,13 +17,16 @@ document.addEventListener('DOMContentLoaded', function() {
   const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
-    
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if(pipePosition <= 120) {
+    console.log(marioPosition);
+     
+ 
+    if(pipePosition <= 120 &&  pipePosition > 0 && marioPosition < 80) {
 
       pipe.style.animation = 'none';
       pipe.style.left = `${pipePosition}px`;
-      ;
+      
 
     }
 
