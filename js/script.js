1aaff0cca356b787f8f7de9406f763e3bb70ed15
WilliamFilh0 <williamfilho8908@gmail.com>
2023-06-08 11:38:23 -0300

    Atualização de imagem Game-Over

--- js/script.js
+++ js/script.js
@@ -21,13 +21,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     console.log(marioPosition);
      
- 
+  
     if(pipePosition <= 120 &&  pipePosition > 0 && marioPosition < 80) {
 
       pipe.style.animation = 'none';
       pipe.style.left = `${pipePosition}px`;
       
-
+      mario.style.animation = 'none';
+      mario.style.bottom = `${marioPosition}px`;
+ 
+      mario.src = './images/game-over.png';
+      mario.style.width = '75px'
+      mario.style.marginLeft = '50px'
     }
 
 
